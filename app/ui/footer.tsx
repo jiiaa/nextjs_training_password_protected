ab622bb6909ccd8bbb95869bdf3a67b66e16d3ce
Jukka Alkunen <jukka.alkunen@terveystalo.com>
2024-08-05 12:26:42 +0300

    feat: Add footer element

---
+++ app/ui/footer.tsx
@@ -0,0 +1,8 @@
+export default function Footer() {
+  return (
+    <footer className="mt-20">
+      <div className="pb-4 border-t-2 border-solid border-my-orange" />
+      <p>© 2024 VowDanger Group</p>
+    </footer>
+  );
+}
