c2172c48ed7aac87bc56505fe4a92e4a72c33aa5
Jukka Alkunen <jukka.alkunen@terveystalo.com>
2024-08-08 16:56:07 +0200

    feat: Add new Google font

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,5 +1,13 @@
-import { Inter, Lusitana } from 'next/font/google'
+import { Inter, Lusitana, Dela_Gothic_One } from "next/font/google";
 
-export const inter = Inter({ subsets: ['latin'] })
+export const inter = Inter({ subsets: ["latin"] });
 
-export const lusitana = Lusitana({ subsets: ['latin'], weight: ['400', '700']})
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
+
+export const delaGothicOne = Dela_Gothic_One({
+  subsets: ["latin"],
+  weight: ["400"],
+});
