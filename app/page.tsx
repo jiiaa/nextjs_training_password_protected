82d9870873eff8babaa414a105f12bed20b4cf26
Jukka Alkunen <jukka.alkunen@terveystalo.com>
2024-08-08 17:15:08 +0200

    feat: Add card component to frontpage

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,63 @@
-import Image from "next/image";
+import {
+  ChevronDoubleUpIcon,
+  CloudIcon,
+  SunIcon,
+} from "@heroicons/react/24/outline";
 import styles from "@/app/ui/home.module.scss";
-import { lusitana } from "./ui/fonts";
+import LocationCard from "@/app/ui/location-card";
+
+const locations = [
+  {
+    title: "Alpit",
+    icon: ChevronDoubleUpIcon,
+    description:
+      "Dolore est pariatur anim laboris. Exercitation ad incididunt commodo occaecat.",
+    image: "/images/alpit.jpg",
+    link: "/protected/alps",
+  },
+  {
+    title: "Saari",
+    icon: CloudIcon,
+    description:
+      "Commodo do esse officia ea nulla cupidatat duis exercitation.",
+    image: "/images/saari.jpg",
+    link: "/protected/island",
+  },
+  {
+    title: "Hiekka",
+    icon: SunIcon,
+    description:
+      "Cupidatat ut minim elit aliquip. Exercitation exercitation proident.",
+    image: "/images/hiekka.jpg",
+    link: "/middleware-protected/sand",
+  },
+];
 
 export default function Home() {
   return (
     <main>
-      <h1>Duis Lorem labore ex aliqua</h1>
-      <div className={styles.shape} />
-      <p
-        className={`${lusitana.className} text-xl text-gray-800 md:text-2xl md:leading-normal`}
-      >
-        Esse ex quis tempor eu et pariatur ex nisi velit nisi officia. Sint
-        dolor duis deserunt aliqua. Occaecat magna commodo excepteur mollit.
-        Occaecat aliqua dolore sint irure id laborum eu pariatur culpa
-        reprehenderit. Non proident ea Lorem mollit veniam mollit. Anim
-        consequat sint nisi est duis elit in proident.
+      <h1>Grata ad nostram deversorium</h1>
+      <p className={styles.intro}>
+        Laboris est cillum sint pariatur aliquip enim. Magna quis officia dolore
+        ea nisi cupidatat qui adipisicing ex voluptate irure. Aliquip proident
+        eu id proident aute ut sit officia in officia irure proident excepteur.
+        Eiusmod velit ea veniam aute do. Sint do fugiat aliquip cillum nisi esse
+        fugiat ea voluptate consequat. Et sit Lorem nostrud ex duis cupidatat do
+        irure nulla elit. Pariatur irure dolor veniam aliqua ipsum qui nisi ad
+        incididunt qui proident minim nulla duis.
       </p>
-      <h2>Alpit</h2>
-      <p>In enim occaecat amet et deserunt laborum deserunt enim sit.</p>
-      <Image
-        src="/images/chamonix.jpg"
-        width={289}
-        height={217}
-        className="hidden md:block"
-        alt="Chamonix the house"
-      />
-      <h2>Saari</h2>
-      <p>Commodo do esse officia ea nulla cupidatat duis exercitation.</p>
+      <section className={styles.locations}>
+        {locations.map((location) => (
+          <LocationCard
+            key={location.title}
+            icon={location.icon}
+            title={location.title}
+            description={location.description}
+            image={location.image}
+            link={location.link}
+          />
+        ))}
+      </section>
     </main>
   );
 }
