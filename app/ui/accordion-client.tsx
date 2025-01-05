4b7b15371682f978867cc41ca11c143f7e4b0e7e
Jukka Alkunen <jukkaa.alkunen@terveystalo.com>
2025-01-05 16:19:59 +0200

    feat: Add HTML/CSS accordion

---
+++ app/ui/accordion-client.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import { useState } from "react";
+import { PlusIcon } from "@heroicons/react/24/outline";
+
+import clsx from "clsx";
+import styles from "./accordion-client.module.scss";
+import Image from "next/image";
+
+export default function AccordionClient(
+  { title, image, children }: { title: string, image: string, children: React.ReactNode }
+) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClick = () => {
+    setIsOpen(!isOpen);
+  }
+
+  const Icon = PlusIcon;
+
+  return (
+    <section>
+      <div className={styles.accordion}>
+        <div onClick={() => handleClick()} className={styles.accordionTitle}>
+          <div>
+            <Icon className={clsx(styles.icon, {[styles.svgRotated]: isOpen})}/>
+            {title}
+          </div>
+        </div>
+        <div className={clsx(styles.accordionContent, {[styles.accordionOpen]: isOpen})}>
+          <div className={styles.contentFlex}>
+            <div className={styles.contentText}>
+              {children}
+            </div>
+            {image &&
+              <div className={styles.contentImage}>
+                <Image
+                  src={image}
+                  width={650}
+                  height={400}
+                  alt="Image"
+                />
+              </div>
+            }
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
