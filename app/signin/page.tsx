ceb3d46236f6dbf4d351a111b3992af5ee2e020f
Jukka Alkunen <jukkaa.alkunen@terveystalo.com>
2025-01-06 16:38:19 +0200

    feat: Style the sign-in page

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -17,13 +17,16 @@ export default async function SignIn() {
   const csrfToken = cookieStore.get(csrfTokenCookie)?.value.split('|')[0];
 
   return (
-    <form method="post" action="/api/auth/callback/credentials">
-      <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
-      <label>
-        Password
-        <input name="password" type="password" />
-      </label>
-      <button type="submit">Sign in</button>
-    </form>
+    <main>
+      <h1 className="text-base text-left">Sign in to see the password protected content.</h1>
+      <form method="post" action="/api/auth/callback/credentials">
+        <input name="csrfToken" type="hidden" defaultValue={csrfToken} />
+        <label>
+          Password: 
+          <input name="password" type="password" className="ml-2 mr-4 text-black bg-my-orange focus:bg-white" />
+        </label>
+        <button type="submit" className="border-solid border-2 border-my-orange py-px px-5 hover:scale-110 active:scale-100">Sign in</button>
+      </form>
+    </main>
   );
 }
