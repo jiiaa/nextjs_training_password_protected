4a68a170262daf0565d0ed29d1111e09262bf844
Jukka Alkunen <jukkaa.alkunen@terveystalo.com>
2025-01-27 21:03:06 +0200

    fix: Fix issue with NextUI components

---
+++ app/protected/sand/page.tsx
@@ -0,0 +1,56 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import AccordionClient from "@/app/ui/accordion-client";
+import styles from "./styles.module.scss";
+import Content from "@/app/content/content.json";
+
+export default async function Page() {
+  // console.log(Content.alps[0]);
+
+  const cookieStore = await cookies();
+  const cookieAuth = cookieStore.get("vowdanger_sand") || null;
+
+  if (!cookieAuth) {
+    redirect("/signin?redirect=/protected/sand");
+  }
+
+  return (
+    <main>
+      <title>The Sand | VoWDanger</title>
+      <h1>The Sand</h1>
+      <div>
+        <p className={styles.address}>
+          Asunnon osoite: {" "}
+          <span>Palm Jumeirah, Dubai United Arab Emirates</span>
+        </p>
+        <p className={styles.text}>
+          <span>Accordion made of basic HTML elements and SCSS.</span> Ex incididunt velit voluptate aliquip aute do ipsum. Nostrud amet excepteur duis aliquip et aute occaecat commodo laboris deserunt velit ad occaecat quis. In velit minim in ut exercitation minim ex Lorem cupidatat deserunt dolore sint laboris aliqua. Culpa tempor tempor officia dolore est incididunt. Reprehenderit nisi aliqua consequat minim sunt eu amet ipsum proident occaecat.
+        </p>
+      </div>
+      <AccordionClient
+        title='Urheilumahdollisuudet'
+        image="/images/sandboarding.jpg"
+      >
+        Esse aliqua quis nisi adipisicing ea incididunt esse pariatur adipisicing proident qui elit aliquip eu. Pariatur sit laborum aute est Lorem commodo elit. Magna Lorem sint eiusmod adipisicing qui adipisicing dolore et ex dolor. Reprehenderit cillum cillum sint ullamco reprehenderit. Deserunt duis culpa labore officia nulla. Tempor eu et fugiat reprehenderit non occaecat. Excepteur in excepteur occaecat id consectetur.
+      </AccordionClient>
+      <AccordionClient
+        title="Est amet incididunt commodo ex excepteur minim adipisicing ea sint"
+        image=""
+      >
+        <AccordionClient
+          title="Minim incididunt consectetur cillum ullamco reprehenderit laboris voluptate sint enim aliquip reprehenderit"
+          image=""
+        >
+          Pariatur qui sunt duis laboris enim consequat cillum Lorem pariatur velit adipisicing id. Duis nisi non irure laboris ullamco consectetur. Consectetur pariatur enim deserunt fugiat veniam ullamco minim incididunt. Tempor elit non quis et anim esse consectetur exercitation amet nostrud. Anim aliquip pariatur pariatur enim fugiat cupidatat tempor sunt id. In magna dolore ea excepteur fugiat id occaecat ea labore quis aute elit. Minim dolore incididunt commodo sint.
+        </AccordionClient>
+        <AccordionClient
+          title="Labore sit consequat labore quis"
+          image=""
+        >
+          Amet cupidatat dolore aliqua commodo. Cupidatat sit dolor labore velit proident. Magna excepteur velit cillum id dolore duis amet adipisicing sunt ea nisi. Elit laborum et anim ea labore aute quis excepteur dolore nulla amet. Deserunt officia non ad ex sint quis. Ad cupidatat in aliqua anim ut aliquip ipsum mollit exercitation excepteur dolor exercitation ut nulla. Exercitation commodo enim nisi non ullamco id exercitation magna consequat esse do ex commodo.
+        </AccordionClient>
+      </AccordionClient>
+    </main>
+  );
+}
---
+++ app/ui/accordion-nextui.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import Image from "next/image";
+import { Accordion, AccordionItem } from "@nextui-org/accordion";
+
+export default function AccordionNextUi() {
+  const itemClasses = {
+    base: "bg-my-blue text-white border border-my-orange mb-4",
+    title: "text-my-orange uppercase",
+    subtitle: "text-my-orange",
+    inidicator: "stroke-white",
+  };
+
+  return (
+    <Accordion variant="splitted" itemClasses={itemClasses}>
+      <AccordionItem key="1" className="pb-2" title="Lorem ipsum tempor quis" subtitle="Occaecat tempor nulla nostrud cupidatat reprehenderit irure nulla.">
+        <div className="flex flex-col md:flex-row gap-x-4 gap-y-4">
+          <div className="basis-1/2">
+            Non sint do dolor nulla commodo nisi est voluptate aliquip duis amet eu adipisicing est. Exercitation dolor sint mollit quis ea fugiat enim labore et aute eu est. Consectetur ea ipsum labore occaecat est ipsum amet velit. Esse deserunt proident do aliqua consectetur veniam non ad.
+          </div>
+          <div className="grow">
+            <Image src="/images/saari.jpg" width={793} height={595} alt="inlet" />
+          </div>
+        </div>
+      </AccordionItem>
+      <AccordionItem key="2" title="Quis velit ex laboris commodo" subtitle="Et aliquip voluptate quis fugiat anim deserunt fugiat.">
+        <Accordion variant="splitted" itemClasses={itemClasses}>
+          <AccordionItem key="1" className="pb-2" title="Lorem ipsum tempor quis" subtitle="Occaecat tempor nulla nostrud cupidatat reprehenderit irure nulla.">
+            <div className="flex flex-col md:flex-row gap-x-4 gap-y-4">
+              <div className="basis-1/2">
+                Non sint do dolor nulla commodo nisi est voluptate aliquip duis amet eu adipisicing est. Exercitation dolor sint mollit quis ea fugiat enim labore et aute eu est. Consectetur ea ipsum labore occaecat est ipsum amet velit. Esse deserunt proident do aliqua consectetur veniam non ad.
+              </div>
+              <div className="grow">
+                <Image src="/images/saari.jpg" width={793} height={595} alt="inlet" />
+              </div>
+            </div>
+          </AccordionItem>
+          <AccordionItem key="2" title="Quis velit ex laboris commodo" subtitle="Et aliquip voluptate quis fugiat anim deserunt fugiat.">
+            Mollit cillum in deserunt esse esse aute ut laborum voluptate et. Dolore labore ex aute laboris. Laboris magna occaecat pariatur nulla minim excepteur officia. Quis non adipisicing ad adipisicing. Pariatur proident esse mollit veniam consectetur sunt laborum voluptate veniam consequat. Est est laboris est culpa reprehenderit voluptate.
+          </AccordionItem>
+      </Accordion>
+      </AccordionItem>
+    </Accordion>
+  );
+}
