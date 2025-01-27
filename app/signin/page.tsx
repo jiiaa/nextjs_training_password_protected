621dd7b9b161dff30e3208cf0c180a8cae12dc91
Jukka Alkunen <jukkaa.alkunen@terveystalo.com>
2025-01-27 21:01:07 +0200

    feat: Improve styles and error handling

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -16,11 +16,11 @@ export default function SignIn() {
       <form action={formAction}>
         <label>
           Password: 
-          <input name="password" type="password" className="ml-2 mr-4 text-black bg-my-orange focus:bg-white" />
+          <input name="password" type="password" className="ml-2 mr-4 mb-4 md:mb-0 pl-1 text-black bg-my-orange focus:bg-white" />
           <input type="hidden" name="redirect" value={searchRedirect} />
         </label>
         <button type="submit" className="border-solid border-2 border-my-orange py-px px-5 hover:scale-110 active:scale-100">Sign in</button>
-        <p>{state?.message}</p>
+        {state?.message && <p className="mt-2 text-white">{state?.message} !</p>}
       </form>
     </main>
   );
