58a3fa5c7408f98fd38beee05fe461615ea93505
Jukka Alkunen <jukkaa.alkunen@terveystalo.com>
2025-04-09 16:31:17 +0300

    chore: Refactor suspense boundary

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -7,30 +7,31 @@ import { handleSignin } from "@/app//lib/actions";
 
 const initialState = { message: "", };
 
-function Signin() {
-  const [state, formAction] = useFormState(handleSignin, initialState);
+function Redirect() {
   const searchRedirect = useSearchParams().get("redirect") || "/";
 
   return (
-    <main>
-      <h1 className="text-base text-left">Sign in to see the password protected content.</h1>
-      <form action={formAction}>
-        <label>
-          Password: 
-          <input name="password" type="password" className="ml-2 mr-4 mb-4 md:mb-0 pl-1 text-black bg-my-orange focus:bg-white" autoFocus />
-          <input type="hidden" name="redirect" value={searchRedirect} />
-        </label>
-        <button type="submit" className="border-solid border-2 border-my-orange py-px px-5 hover:scale-110 active:scale-100">Sign in</button>
-        {state?.message && <p className="mt-2 text-white">{state?.message} !</p>}
-      </form>
-    </main>
+    <input type="hidden" name="redirect" value={searchRedirect} />
   );
 }
 
 export default function SignInPage () {
+  const [state, formAction] = useFormState(handleSignin, initialState);
+
   return (
-    <Suspense fallback={<p>Loading...</p>}>
-      <Signin />
-    </Suspense>
+      <main>
+        <h1 className="text-base text-left">Sign in to see the password protected content.</h1>
+        <form action={formAction}>
+          <label>
+            Password:
+            <input name="password" type="password" className="ml-2 mr-4 mb-4 md:mb-0 pl-1 text-black bg-my-orange focus:bg-white" autoFocus />
+            <Suspense>
+              <Redirect />
+            </Suspense>
+          </label>
+          <button type="submit" className="border-solid border-2 border-my-orange py-px px-5 hover:scale-110 active:scale-100">Sign in</button>
+          {state?.message && <p className="mt-2 text-white">{state?.message} !</p>}
+        </form>
+      </main>
   );
 }
